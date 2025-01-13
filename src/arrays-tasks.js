ddb6148f0dc7a6d05906c7b09bef5993790ae2dc
fireleit <fireleit@gmail.com>
2025-01-13 17:40:29 +0400

    feat: add solution to function createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -333,8 +333,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize, result = []) {
+  const newArr = arr.splice(0, chunkSize);
+
+  result.push(newArr);
+
+  return arr.length <= 0 ? result : createChunks(arr, chunkSize, result);
 }
 
 /**
