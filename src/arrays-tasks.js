e569cd35daf72dec08bfa965e094304be67995e5
fireleit <fireleit@gmail.com>
2025-01-13 17:41:09 +0400

    feat: add solution to function generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -353,8 +353,9 @@ function createChunks(arr, chunkSize, result = []) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = Array(len).fill(0);
+  return arr.map((el, i) => i * 2 + 1);
 }
 
 /**
